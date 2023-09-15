4ac7b444179caaa10e3018be48736fa928687c89
gabri <gabriellondero40@gmail.com>
2023-09-15 05:56:52 -0300

    first commit

--- src/main/java/edu/Biblioteca/Biblioteca1/servicios/EstudianteServicio.java
+++ src/main/java/edu/Biblioteca/Biblioteca1/servicios/EstudianteServicio.java
@@ -11,21 +11,21 @@ public class EstudianteServicio {
 
     @Autowired
     EstudianteRepositorio estudianteRepositorio;
-
+    //Traer todos los estudiante en una lista
     public List<Estudiante> getAll() {
         List<Estudiante> lista = new ArrayList<Estudiante>();
         estudianteRepositorio.findAll().forEach(registro -> lista.add(registro));
         return lista;
     }
-
+    //Traer estudiante por id
     public Estudiante getById(Long id) {
         return estudianteRepositorio.findById(id).get();
     }
-
+    //Editar o actualizar estudiante
     public void save(Estudiante estudiante) {
         estudianteRepositorio.save(estudiante);
     }
-
+    //Eliminar estudiante
     public void delete(Long id) {
         estudianteRepositorio.deleteById(id);
     }
